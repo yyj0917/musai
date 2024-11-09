0c1dbfdaf5b93af5869fd068781f52d742ae4138
yyj0917 <yyj0917@yonsei.ac.kr>
2024-11-09 23:41:55 +0900

    Style : 타이포 시스템 tailwind 설정, layout.tsx 폰트 설정

--- pedalgenie/src/app/layout.tsx
+++ pedalgenie/src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +13,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        {/* Pretendard 웹폰트 CDN 설정 */}
+        <link
+          rel="stylesheet"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.8/dist/web/static/pretendard-dynamic-subset.css"
+          crossOrigin="anonymous"
+        />
+      </head>
+      <body>
         {children}
       </body>
     </html>
