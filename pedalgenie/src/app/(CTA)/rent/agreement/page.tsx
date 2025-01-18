f9a0186621fd0dfb2fad8ca1dd0c79148f7356d4
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-18 16:53:46 +0900

    style: agreement 페이지 스크롤 수정

--- pedalgenie/src/app/(CTA)/rent/agreement/page.tsx
+++ pedalgenie/src/app/(CTA)/rent/agreement/page.tsx
@@ -6,15 +6,17 @@ import PriceSummary from '../_components/paymentSummary';
 
 export default function Agreement() {
   return (
-    <div className="w-full flex flex-col text-grey250 font-pretendard">
+    <div className="w-full flex flex-col text-grey250 font-pretendard ">
       <TopBar />
-      <div className='p-4 border-b-0.5 border-grey850'>
-        <ReservationInfo />
-        <PriceSummary />
-      </div>
-      <div className='p-4'>
-        <NoticeSection />
-        <AgreementSection/>
+      <div className='w-full h-[calc(100vh-50px)] overflow-y-auto scroll-smooth scrollbar-hide '>
+        <div className='p-4 border-b-0.5 border-grey850'>
+          <ReservationInfo />
+          <PriceSummary />
+        </div>
+        <div className='p-4'>
+          <NoticeSection />
+          <AgreementSection/>
+        </div>
       </div>
     </div>
   );
