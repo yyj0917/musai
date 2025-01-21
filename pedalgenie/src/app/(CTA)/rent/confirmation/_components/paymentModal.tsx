442c09907c9360ff30237d74c809568b39e6421d
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-21 18:58:10 +0900

    fix: useRouter 에러 수정

--- pedalgenie/src/app/(CTA)/rent/confirmation/_components/paymentModal.tsx
+++ pedalgenie/src/app/(CTA)/rent/confirmation/_components/paymentModal.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { Button } from '@/components/ui/button';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import CopyButton from './copyButton';
 
 interface PaymentModalProps {
