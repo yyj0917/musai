4b948ff7166c1e2b70bf2405996dba1adc322631
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-23 20:41:51 +0900

    feat: product Id에 따라 {예약하기} 페이지 이동

--- pedalgenie/src/app/(CTA)/demo/page.tsx
+++ pedalgenie/src/app/(CTA)/demo/page.tsx
@@ -1,20 +1,20 @@
-import Calendar from "./_components/calendar";
-import TimePicker from "./_components/timePicker";
-import TopBar from "./_components/topBar";
+import Calendar from './_components/calendar';
+import TimePicker from './_components/timePicker';
+import TopBar from './_components/topBar';
 import AlertCircle from '@public/svg/rent/alert-circle.svg';
 
-export default function Rent() {
+export default function Demo() {
   return (
     <div className="w-full h-full flex flex-col bg-grey1000 text-grey250 font-pretendard">
       <TopBar />
       <div className="w-full h-[calc(100vh-50px)] overflow-y-auto scroll-smooth scrollbar-hide ">
-      <div className="p-4">
-        <div className="text-title2 pb-3">시연 날짜</div>
-        <Calendar />
-      </div>
-      <section className="w-full p-4 border-t-0.5 border-grey850">
-        <TimePicker />
-      </section> 
+        <div className="p-4">
+          <div className="text-title2 pb-3">시연 날짜</div>
+          <Calendar />
+        </div>
+        <section className="w-full p-4 border-t-0.5 border-grey850">
+          <TimePicker />
+        </section>
       </div>
     </div>
   );
--- pedalgenie/src/app/(CTA)/product/[id]/page.tsx
+++ pedalgenie/src/app/(CTA)/product/[id]/page.tsx
@@ -12,45 +12,53 @@ import ShopInfo from './_components/shopInfo';
 import InfoSwitcher from './_components/infoSwitcher';
 // 하트 버튼 컴포넌트로 따로 뺴기
 import Heart from '@public/svg/product/heart.svg';
+import CTA from '@/components/ui/CTA';
 
 export default function Product({ params }: { params: { id: number } }) {
   const { id } = params; // 파라미터로 받아온 ProductId 값
 
-  const { data: productDetail, isLoading, isError } = useQuery({
+  const {
+    data: productDetail,
+    isLoading,
+    isError,
+  } = useQuery({
     queryKey: ['productDetail', id], // 캐싱 키
     queryFn: () => fetchProductDetail(id), // fetchShopDetail 함수 호출
     staleTime: 1000 * 60 * 5, // 5분 동안 데이터 신선 상태 유지
   });
 
   return (
-    <div className="w-full flex flex-col text-grey250 font-pretendard">
-      <TopBar />
-      <div className="w-full h-[calc(100vh-48px-87px)] overflow-y-auto scroll-smooth scrollbar-hide pb-10">
-        <ProductImg productImg={productDetail?.productImage} />
-        <ShopNameBar shopName={productDetail?.shopName} shopId={productDetail?.shopId} />
-        <div className="px-4 pb-0 py-[14px]">
-          <ProductActionMenus
-            isRentable={productDetail?.isRentable}
-            isPurchasable={productDetail?.isPurchasable}
-            isDemoable={productDetail?.isDemoable}
+    <div className="relative w-full h-full bg-grey1000">
+      <div className="w-full flex flex-col text-grey250 font-pretendard">
+        <TopBar />
+        <div className="w-full h-[calc(100vh-48px-87px)] overflow-y-auto scroll-smooth scrollbar-hide pb-10">
+          <ProductImg productImg={productDetail?.productImage} />
+          <ShopNameBar shopName={productDetail?.shopName} shopId={productDetail?.shopId} />
+          <div className="px-4 pb-0 py-[14px]">
+            <ProductActionMenus
+              isRentable={productDetail?.isRentable}
+              isPurchasable={productDetail?.isPurchasable}
+              isDemoable={productDetail?.isDemoable}
+            />
+            <section className="flex space-x-[9px]">
+              <div className="flex w-full font-semibold text-lg pb-5 max-w-[310px] break-words">
+                {productDetail?.name}
+              </div>
+              <Heart />
+            </section>
+            <ProductFeeCard price={productDetail?.price} rentPricePerDay={productDetail?.rentPricePerDay} />
+            <InfoSwitcher />
+          </div>
+          <DescriptionImg descriptionImg={productDetail?.descriptionUrl} />
+          <ShopInfo
+            shopName={productDetail?.shopName}
+            shopHours={productDetail?.shopHours}
+            contactNumber={productDetail?.contactNumber}
+            address={productDetail?.address}
           />
-          <section className="flex space-x-[9px]">
-            <div className="flex w-full font-semibold text-lg pb-5 max-w-[310px] break-words">
-              {productDetail?.name}
-            </div>
-            <Heart />
-          </section>
-          <ProductFeeCard price={productDetail?.price} rentPricePerDay={productDetail?.rentPricePerDay} />
-          <InfoSwitcher />
         </div>
-        <DescriptionImg descriptionImg={productDetail?.descriptionUrl} />
-        <ShopInfo
-          shopName={productDetail?.shopName}
-          shopHours={productDetail?.shopHours}
-          contactNumber={productDetail?.contactNumber}
-          address={productDetail?.address}
-        />
       </div>
+      <CTA id={id} />
     </div>
   );
 }
