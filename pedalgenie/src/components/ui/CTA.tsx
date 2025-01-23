7142ce690ad833884a604faf27256b35f7ca6df1
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-23 19:11:53 +0900

    feat: CTA Link 수정

--- pedalgenie/src/components/ui/CTA.tsx
+++ pedalgenie/src/components/ui/CTA.tsx
@@ -16,11 +16,11 @@ export default function CTA() {
   // }, [pathname]);
   return (
     <nav className="bg-grey1000 absolute bottom-0 w-full flex justify-between items-center pt-3 pb-[50px] px-[20px] border-t-[1px] border-grey750 font-pretendard">
-      <CTAHeart/>
+      <CTAHeart />
       <div className="flex justify-center items-center gap-[26px] text-label1 text-grey250">
         <Link href={'/rent'}>대여 예약하기</Link>
-        <CTAvector/>
-        <Link href={''}>시연 예약하기</Link>
+        <CTAvector />
+        <Link href={'/demo'}>시연 예약하기</Link>
       </div>
     </nav>
   );
