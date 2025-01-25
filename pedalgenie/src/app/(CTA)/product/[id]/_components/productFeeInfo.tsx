db7c3eceda35205afa5faf90527f497d9f9b51ce
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-25 15:06:49 +0900

    style: 상품 상세 페이지 가격 카드 수정
    1. 하단 설명 삭제
    2. 대여 부분 '일 | ' 추가

--- pedalgenie/src/app/(CTA)/product/[id]/_components/productFeeInfo.tsx
+++ pedalgenie/src/app/(CTA)/product/[id]/_components/productFeeInfo.tsx
@@ -13,16 +13,17 @@ export default function ProductFeeCard({ price, rentPricePerDay }: ProductFeeCar
     <section className="flex w-full space-x-[7px] pb-5">
       {/* 구매료 표시 부분 */}
       <div className="flex flex-col w-full bg-grey950 rounded px-4 py-3.5">
-        <p className="text-grey550 font-semibold text-sm pb-2">구매</p>
+        <p className="text-grey550 text-body2 pb-2">구매</p>
         <p className="text-white font-semibold">{formatPrice(price)}원</p>
-        <p className="text-grey550 text-xs">매장에 직접 구매 문의</p>
       </div>
-          
       {/* 대여료 표시 부분 */}
       <div className="flex flex-col w-full bg-grey950 rounded px-4 py-3.5">
-        <p className="text-grey550 font-semibold text-sm pb-2">대여</p>
-        <p className="text-white font-semibold">{formatPrice(rentPricePerDay)}원</p>
-        <p className="text-grey550 text-xs">수수료 별도 10% 부가</p>
+        <p className="text-grey550 text-body2 pb-2">대여</p>
+        <span className="flex">
+          <p className="text-label1 text-grey550">일</p>
+          <p className="flex items-center text-label1 !text-xs text-grey550 px-1">|</p>
+          <p className="text-white font-semibold">{formatPrice(rentPricePerDay)}원</p>
+        </span>
       </div>
     </section>
   );
