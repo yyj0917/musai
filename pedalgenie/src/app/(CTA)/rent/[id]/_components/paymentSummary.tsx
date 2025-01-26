42db89e8fe4d5720e99ad9da766309c0c30c3497
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-26 12:25:19 +0900

    fix: 디자인 QA 수정

--- pedalgenie/src/app/(CTA)/rent/[id]/_components/paymentSummary.tsx
+++ pedalgenie/src/app/(CTA)/rent/[id]/_components/paymentSummary.tsx
@@ -6,16 +6,15 @@ import React, { useEffect, useState } from 'react';
   /* API 데이터로 수정 */
 }
 
-export default function PaymentSummary() {
+export default function PaymentSummary({}) {
   return (
     <section className="w-full">
-      <div className="pt-3 flex justify-between">
-        <p className="text-grey550">30,000원 x 10일</p>
-        <p className="text-grey150">300,000원</p>
-      </div>
-      <div className="flex justify-between pb-3 border-b-0.5 border-grey850">
-        <p className="text-grey550">수수료</p>
-        <p className="text-grey150">3,000원</p>
+      <div className="pt-3 pb-3 border-b-0.5 border-grey850 flex justify-between">
+        <span className='flex-col'>
+        <p className="text-grey550 text-label2">30,000원</p>
+        <p className="text-grey550 ">x 10일</p>
+        </span>
+        <p className="text-label1 text-grey150">300,000원</p>
       </div>
       <div className="flex justify-between pt-3 pb-1">
         <p className="text-title1 text-red">합계</p>
