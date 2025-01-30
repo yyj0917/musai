e0a60fdaac4fbd66ab2a33cec1b01b1b04dca73c
Jisu <92006284+jsomnium@users.noreply.github.com>
2025-01-30 16:33:03 +0900

    refactor: layout 폴더 안쪽으로 수정

---
+++ pedalgenie/src/app/(CTA)/demo/[id]/layout.tsx
@@ -0,0 +1,7 @@
+export default function DemoLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return <div className="relative w-full h-full bg-grey1000">{children}</div>;
+  }
